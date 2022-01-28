139314e1011f55dc8f269a31402b459e3362fd66
AlexandreHobbes <alexandrehobbes@gmail.com>
2022-01-28 19:17:05 -0300

    Segundo Exercício

--- conexaoDAO.php
+++ conexaoDAO.php
@@ -1,5 +1,5 @@
 <?php
-    include "tenistas.php";
+    include "jogos.php";
     class ConexaoDAO
     {
         private $conexao;
@@ -14,23 +14,34 @@
         }
         public function getAll()
         {
-            $tenistas = [];
+            $jogos = [];
 
-            $resultado = mysqli_query($this->conexao, "SELECT * FROM tenistas");
-            $numero_de_tenistas = mysqli_num_rows($resultado);
-            for($i = 0; $i < $numero_de_tenistas; $i++)
+            $resultado = mysqli_query($this->conexao, 
+            "SELECT j.tenista_01_id, t1.nome j.tenista_02_id, t2.nome, j.campeonatos_id, j.publico, j.pontuacao_tenista_01, j.pontuacao_tenista_02,
+            j.quadras_id, q.tipo
+            FROM jogos j 
+            JOIN tenistas t1 ON j.tenista_01_id LIKE t1.id
+            JOIN tenistas t2 ON j.tenista_02_id LIKE t2.id
+            JOIN quadras q ON j.quadras_id LIKE q.id");
+            $numero_de_jogos = mysqli_num_rows($resultado);
+            for($i = 0; $i < $numero_de_jogos; $i++)
             {
                 $row = mysqli_fetch_array($resultado);
-                $new_tenista = new Tenistas
+                $new_jogo = new Jogos
                 (
-                    $row["id"],
-                    $row["nome"],
-                    $row["data_nascimento"],
-                    $row["sexo"],
-                    $row["categorias_id"]
+                    $row[0],
+                    $row[1],
+                    $row[2],
+                    $row[3],
+                    $row[4],
+                    $row[5],
+                    $row[6],
+                    $row[7],
+                    $row[8],
+                    $row[9]
                 );
 
-                $tenistas[] = $new_tenista;
+                $jogo[] = $new_jogo;
             }
         }
     }
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     $conexao = new Conexao();
     $conexaoDAO = new ConexaoDAO($conexao->getConnection());
 
-    $tenistas = $conexaoDAO->getAll();
+    $jogos = $conexaoDAO->getAll();
     
 ?>
 
@@ -14,24 +14,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    table, th, td 
+    {
+        border: 1px solid black;
+    }
+    </style>
     <title>E12-PHP-SQL</title>
 </head>
 <body>
     <h1>Federação Tenis</h1>
+    <table>
+        <tr>
+            <th>ID tenista 01</th>
+            <th>Nome tenista 01</th>
+            <th>ID tenista 02</th>
+            <th>Nome tenista 02</th>
+            <th>ID campeonato</th>
+            <th>Pontuacao tenista 01</th>
+            <th>Pontuacao tenista 02</th>
+            <th>ID Quadra</th>
+            <th>Tipo de Quadra</th>
+        </tr>
     <?php
-        foreach($tenista as $tenistas)
+        foreach($jogo as $jogos)
         {
     ?>
-            <p>
-                <?= $tenista->get_id()?>
-                <?= $tenista->get_nome()?>
-                <?= $tenista->get_data_nascimento()?>
-                <?= $tenista->get_sexo()?>
-                <?= $tenista->get_categorias_id()?>
-            </p>
+        <tr>
+            <td><?= $jogo->get_tenista_01_id()?></td>
+            <td><?= $jogo->get_nome1()?></td>
+            <td><?= $jogo->get_tenista_02_id()?></td>
+            <td><?= $jogo->get_nome2()?></td>
+            <td><?= $jogo->get_campeonatos_id()?></td>
+            <td><?= $jogo->get_pontuacao_tenista_01()?></td>
+            <td><?= $jogo->get_pontuacao_tenista_02()?></td>
+            <td><?= $jogo->get_quadras_id()?></td>
+            <td><?= $jogo->get_tipoQ()?></td>
+            
+        </tr>
     <?php
         }
     ?>
+    </table>
 
 
 </body>
