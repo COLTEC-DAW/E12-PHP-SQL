e4e16f1e2249b4eb22cfcc5b113d3c241d3124c1
Iago Diniz Martins <67806966+IagoDinizMartins@users.noreply.github.com>
2022-01-28 08:51:23 -0300

    add banco de dados

---
+++ PHPeSQLexercicio.sql
@@ -0,0 +1,49 @@
+	CREATE DATABASE nova_atividade;
+    USE nova_atividade;
+    
+    CREATE TABLE categorias(
+		id INT,
+        nome VARCHAR(45),
+        CONSTRAINT pk_categorias PRIMARY KEY(id)	
+    );
+    CREATE TABLE tenistas(
+		id INT,
+        nome VARCHAR(150),
+        data_nascimento DATETIME,
+        sexo BIT,
+        categorias_id INT,
+        CONSTRAINT pk_tenistas PRIMARY KEY(id),
+        CONSTRAINT fk_tenistas_categorias FOREIGN KEY(categorias_id) REFERENCES categorias (id)
+    );
+    
+     CREATE TABLE quadras(
+		id INT,
+        tipo VARCHAR(45),
+        endereco VARCHAR(45),
+        CONSTRAINT pk_quadras PRIMARY KEY(id)	
+    );
+      CREATE TABLE campeonatos(
+		id INT,
+        nome VARCHAR(150),
+        edicao VARCHAR(45),
+        data_realizacao DATETIME,
+        premiacao DECIMAL(10,2),
+        CONSTRAINT pk_campeonatos PRIMARY KEY(id)	
+    );
+    CREATE TABLE jogos(
+		tenista_01_id INT,
+        tenista_02_id INT,
+        campeonatos_id int,
+        publico INT,
+        pontuacao_tenista_01 INT,
+        pontuacao_tenista_02 INT,
+        quadras_id INT,
+        
+        CONSTRAINT pk_jogos PRIMARY KEY (tenista_01_id, tenista_02_id, campeonatos_id),
+		CONSTRAINT fk_jogos_tenistas_01 FOREIGN KEY(tenista_01_id) REFERENCES tenistas(id),
+        CONSTRAINT fk_jogos_tenistas_02 FOREIGN KEY(tenista_02_id) REFERENCES tenistas(id),
+        CONSTRAINT fk_jogos_campeonatos FOREIGN KEY(campeonatos_id) REFERENCES campeonatos(id),
+        CONSTRAINT fk_jogos_quadras FOREIGN KEY(quadras_id) REFERENCES quadras(id)
+    );
+    show tables;
+    desc jogos;
